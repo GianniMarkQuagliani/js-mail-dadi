9d7aeec9950963fd2bc06f6b9fee83533f0561f1
GianniMarkQuagliani <giannimarkquagliani@gmail.com>
2023-09-12 15:07:00 +0200

    [add] Check if the user's email is in the allowed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 //Creare un arrey di email consentite
-const emailCostante = ['email1@example.com', 'email2@example.com', 'email3@example.com'];
+const emailConsentite = ['email1@example.com', 'email2@example.com', 'email3@example.com'];
 //Assegnare il pulsante  "Verifica Email" a una variabile
 const verificaEmailBtn = document.getElementById('verificaEmailBtn');
 //Aggiungere un event listener al pulsante  "Verifica Email" per gestire il clic
@@ -7,7 +7,13 @@ verificaEmailBtn.addEventListener('click', function () {
     //Dichiarazione di una variabile che contiene l'email inserita dall'utente.
     const emailUtente = document.getElementById('emailInput').value;
     //Dichiarazione di un elemento DOM che rappresenta il risultato della verifica email.
-    const risultatoEmail = document.getElementById("risultatoEmail");
-    console.log(emailUtente);
-
+    const risultatoEmail = document.getElementById('risultatoEmail');
+    //Verificare se l'email dell'utente è nella lista delle email consentite, poi stampare
+    if (emailConsentite.includes(emailUtente)) {
+        //Se l'email è autorizzata, imposta il risultato della verifica su "Accesso consentito".
+        risultatoEmail.textContent = 'Accesso consentito. Benvenuto!';
+    } else {
+        //Se l'email non è autorizzata, imposta il risultato della verifica su "Accesso negato".
+        risultatoEmail.textContent = 'Accesso negato. La tua email non è autorizzata.';
+    }
 });
